fc08821d46614afc0a4b4ed6f3099488822de4ed
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-04-07 23:59:16 +0530

    Tooltip problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+      <Tooltip text="This is tooltip for Text 1">
+        <p>Hover over me (Text 1)</p>
+      </Tooltip>
+
+      <Tooltip text="Tooltip for Button">
+        <button>Hover Button</button>
+      </Tooltip>
+
+      <Tooltip text="Tooltip for Span">
+        <span>Hover Span</span>
+      </Tooltip>
+
+    </div>
+  );
+};
+
+export default App;
